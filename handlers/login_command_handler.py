2c59f7b86f0d16b0cc64bfc21467b70480344d8f
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-07-09 13:30:07 +0530

    Added user id for logging in

--- handlers/login_command_handler.py
+++ handlers/login_command_handler.py
@@ -8,4 +8,6 @@ def login_command_handler(update: Update, context: CallbackContext):
     telegram_user_id = update.message.chat_id
     generated_otp = Otp(telegram_user_id)
     generated_otp.save()
-    update.message.reply_text(f"OTP for login: {generated_otp.otp}")
+    update.message.reply_text(
+        f"User ID: {generated_otp.telegram_user_id}, OTP: {generated_otp.otp}"
+    )
